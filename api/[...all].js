f9ad5876f8b7aa1fd9a9dc21b70908f3166058b9
Credible Sheets <crediblesheets@gmail.com>
2025-09-10 15:35:06 +0800

    Update index and api

--- api/[...all].js
+++ api/[...all].js
@@ -1,8 +1,8 @@
 // api/[...all].js
 import app from '../server.js';
 
-// Vercel strips the "/api" prefix when calling this function.
-// Put it back so Express routes like "/api/auth/login" will match.
+// Vercel strips the '/api' prefix before calling this function.
+// Put it back so Express routes like '/api/auth/login' match.
 export default function handler(req, res) {
   if (!req.url.startsWith('/api')) {
     req.url = '/api' + (req.url === '/' ? '' : req.url);
